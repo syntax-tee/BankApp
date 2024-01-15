972f3e0f77d34eae5cd3ed803c18162600514046
Taiye Ogunlade <ogunladetaiye@gmail.com>
2024-01-15 13:13:04 -0600

    Update PerfectSquareCount.java

--- src/test/java/com/bankapp/ogunladetaiye/PerfectSquareCount.java
+++ src/test/java/com/bankapp/ogunladetaiye/PerfectSquareCount.java
@@ -1,22 +1,22 @@
-package com.bankapp.ogunladetaiye;
+// package com.bankapp.ogunladetaiye;
 
-public class PerfectSquareCount {
+// public class PerfectSquareCount {
 
-    public static void main(String[] args) {
-        int a = 4, b = 5;
-        PerfectSquareCount obj = new PerfectSquareCount();
-        System.out.print("Count of squares is " + (int) obj.countPerfectSquares(a, b));
-    }
+//     public static void main(String[] args) {
+//         int a = 4, b = 5;
+//         PerfectSquareCount obj = new PerfectSquareCount();
+//         System.out.print("Count of squares is " + (int) obj.countPerfectSquares(a, b));
+//     }
 
-    static int countPerfectSquares(int m, int n)
-    {
-        if (n < m)
-        { int temp = m;
-            m = n;
-            n = temp;
-        }
+//     static int countPerfectSquares(int m, int n)
+//     {
+//         if (n < m)
+//         { int temp = m;
+//             m = n;
+//             n = temp;
+//         }
 
-        return m * (m + 1) * (2 * m + 1) / 6 + (n - m) * m * (m + 1) / 2;
-    }
+//         return m * (m + 1) * (2 * m + 1) / 6 + (n - m) * m * (m + 1) / 2;
+//     }
 
-}
+// }
